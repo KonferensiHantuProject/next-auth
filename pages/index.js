70cebc507735cb307e97472e35587fca648bcd7f
MuhammadNazhimMaulana <nazhimmaulanamuhammad@gmail.com>
2023-10-27 17:32:54 +0700

    Updates

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,16 @@
 import LoginForm from "@/components/LoginForm";
+import { useSession } from "next-auth/react";
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
+  // Router
+  const router = useRouter();
+
+  // Check Session
+  const { data: session } = useSession()
+
+  if(session) router.push("dashboard");
+
   return (
     <main>
       <LoginForm />
